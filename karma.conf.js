71549f4b8357d1c3adf1bb91190d0a3e603bad3d
Ricardo Münch <munch_141@hotmail.com>
2020-09-13 11:08:29 +0200

    adds chrome addon to travis and adds headless flag for chrome in karma

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--headless', '--no-sandbox']
       }
     },
     singleRun: false,
