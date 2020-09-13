b65388b6cf6c5362fdfec41682547baa788c0012
Ricardo Münch <munch_141@hotmail.com>
2020-09-13 16:48:01 +0200

    update tests

--- src/app/components/column/column.component.spec.ts
+++ src/app/components/column/column.component.spec.ts
@@ -18,8 +18,4 @@ describe('ColumnComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
