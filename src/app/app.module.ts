730969599788bb2ca45d34f9227608b9a7b6c7e5
Ricardo Münch <munch_141@hotmail.com>
2020-09-26 13:08:23 +0200

    imports new column into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { ColumnComponent } from "./components/column/column.component";
 import { BoardComponent } from "./components/board/board.component";
 import { PostComponent } from "./components/post/post.component";
 import { AddColumnComponent } from './components/add-column/add-column.component';
+import { NewColumnFormComponent } from './components/new-column-form/new-column-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ColumnComponent, BoardComponent, PostComponent, AddColumnComponent],
-  imports: [BrowserModule, DragDropModule, FontAwesomeModule],
+  declarations: [AppComponent, ColumnComponent, BoardComponent, PostComponent, AddColumnComponent, NewColumnFormComponent],
+  imports: [BrowserModule, DragDropModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
