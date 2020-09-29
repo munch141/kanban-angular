ed71c03f4f1f7762a959b3b40af6d9b4e3904995
Ricardo Münch <munch_141@hotmail.com>
2020-09-29 07:55:29 +0200

    passes create new column form test

--- src/app/components/new-column-form/new-column-form.component.spec.ts
+++ src/app/components/new-column-form/new-column-form.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
 
-import { NewColumnFormComponent } from './new-column-form.component';
+import { NewColumnFormComponent } from "./new-column-form.component";
 
-describe('NewColumnFormComponent', () => {
+describe("NewColumnFormComponent", () => {
   let component: NewColumnFormComponent;
   let fixture: ComponentFixture<NewColumnFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewColumnFormComponent ]
-    })
-    .compileComponents();
+      declarations: [NewColumnFormComponent],
+      imports: [FormsModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +20,7 @@ describe('NewColumnFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
